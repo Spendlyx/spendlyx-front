d7abeeb9dafe63bb543efc4f9c990c396451cd3a
dylan-spreafico <spreafid@gmail.com>
2025-11-09 15:11:10 +0100

    Move model

--- src/app/sidebar/sidebar-items/sidebar-items.ts
+++ src/app/sidebar/sidebar-items/sidebar-items.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { SidebarItem } from "@app/sidebar/sidebar-item/sidebar-item";
-import { SidebarItem as SidebarItemInterface } from "@app/sidebar/sidebar-item/models/sidebar-item.model";
+import { SidebarItem as SidebarItemInterface } from "@app/sidebar/models/sidebar-item.model";
 import { SIDEBAR_ITEMS } from "@app/sidebar/sidebar-items/sidebar-items.config";
 
 @Component({
